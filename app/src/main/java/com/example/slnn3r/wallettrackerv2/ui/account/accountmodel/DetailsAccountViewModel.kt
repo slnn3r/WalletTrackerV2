a12eaa43a172fdad240145f9a6598c96182cbc01
eric.wong <eric.wong@hexainitiative.com>
2018-12-03 13:49:49 +0800

    [ACCOUNT MODULE] (slnn3r) Add Function to delete Account's Transaction when delete Account

--- app/src/main/java/com/example/slnn3r/wallettrackerv2/ui/account/accountmodel/DetailsAccountViewModel.kt
+++ app/src/main/java/com/example/slnn3r/wallettrackerv2/ui/account/accountmodel/DetailsAccountViewModel.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.example.slnn3r.wallettrackerv2.constant.Constant
 import com.example.slnn3r.wallettrackerv2.data.objectclass.Account
 import com.example.slnn3r.wallettrackerv2.data.realmclass.AccountRealm
+import com.example.slnn3r.wallettrackerv2.data.realmclass.TransactionRealm
+import com.google.gson.Gson
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -51,6 +53,35 @@ class DetailsAccountViewModel : AccountModelInterface.DetailsAccountViewModel {
 
             accountRealm.forEach { accountRealmData ->
                 accountRealmData.deleteFromRealm()
+            }
+        }
+        realm.close()
+
+        deleteAccountTransaction(mContext, accountId)
+    }
+
+    private fun deleteAccountTransaction(mContext: Context, accountId: String) {
+        val realm: Realm?
+        Realm.init(mContext)
+
+        val config = RealmConfiguration.Builder()
+                .name(Constant.RealmTableName.TRANSACTION_REALM_TABLE)
+                .build()
+
+        realm = Realm.getInstance(config)
+
+        realm!!.executeTransaction {
+            val transactionRealm =
+                    realm.where(TransactionRealm::class.java).findAll()
+
+            transactionRealm.forEach { transactionRealmData ->
+                val gson = Gson()
+                val accountJson = transactionRealmData.account
+                val accountData = gson.fromJson<Account>(accountJson, Account::class.java)
+
+                if (accountData.accountId.equals(accountId, ignoreCase = true)) {
+                    transactionRealmData.deleteFromRealm()
+                }
             }
         }
         realm.close()
